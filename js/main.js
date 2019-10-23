d849642879676644b3f3ba3bbacc7543fcae60ef
rodrigograca31 <rodrigograca1993@gmail.com>
2019-10-23 17:02:15 +0100

    Animations + Favicon

--- js/main.js
+++ js/main.js
@@ -19,23 +19,48 @@ setRandomClass = () => {
 	items[random].setAttribute("class", "blink " + colors[Math.floor(Math.random() * 5)]);
 }
 
+const isInViewport = (elem) => {
+	const bounding = elem.getBoundingClientRect();
+	return (
+		bounding.top >= 0 &&
+		bounding.left >= 0 &&
+		bounding.bottom-200 <= (window.innerHeight || document.documentElement.clientHeight) &&
+		bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+const loopImages = () => {
+	[...document.getElementsByTagName("img")].forEach(function name(ele) {
+		if (isInViewport(ele)) {
+			ele.classList.add("animate")
+		}
+	})
+}
+
 window.addEventListener("load", (event) => {
 	setInterval(() => {
 		setRandomClass();
 	}, 500);
 
 	document.getElementById("menu").addEventListener("click", (ele) => {
-		console.log(ele.target);
 		
 		if (ele.target.getAttribute("src").indexOf("close") === -1) {
-			console.log(ele.target.setAttribute("src", "./imgs/close.svg"));
+			ele.target.setAttribute("src", "./imgs/close.svg");
 		} else {
-			console.log(ele.target.setAttribute("src", "./imgs/bars.svg"));
+			ele.target.setAttribute("src", "./imgs/bars.svg");
 		}
 		
-		
-		console.log(document.getElementsByTagName("nav")[0].classList);
 		document.getElementsByTagName("nav")[0].classList.toggle("open")
 	})
+
+	window.addEventListener("scroll", () => {
+		loopImages();
+	});
+	window.addEventListener("resize", () => {
+		loopImages();
+	});
+
+	loopImages();
 });
+
 
